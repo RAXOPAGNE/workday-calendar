797bf03e6a462f7cf3731443a609aef5f649ba63
Raxopagne <OskarZaorski1@gmail.com>
2023-02-23 16:45:04 +0000

    Got rid of hard coded table

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+//clock
+function displayTime(){
+    var dateTime = new Date();
+    var hrs = dateTime.getHours();
+    var min = dateTime.getMinutes();
+    var sec = dateTime.getSeconds();
+    var session = document.getElementById('session');
 
+    if(hrs >= 12){
+        session.innerHTML = 'PM';
+    } else {
+        session.innerHTML = 'AM';
+    }
+
+    document.getElementById('hours').innerHTML = hrs;
+    document.getElementById('minutes').innerHTML = min;
+    document.getElementById('seconds').innerHTML = sec;
+}
+setInterval(displayTime, 10);
+
+//local storage
+var addLocation = (selectedLocation) => {
+    recentLocations.push(selectedLocation);
+    console.log(recentLocations)
+  
+    localStorage.setItem("recentLocations", JSON.stringify(recentLocations));
+  }

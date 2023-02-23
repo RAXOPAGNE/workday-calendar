7ffafa27ccac80b96971e5f2074785475fba6b3b
Raxopagne <OskarZaorski1@gmail.com>
2023-02-23 17:00:51 +0000

    created update allocatoin function

--- script.js
+++ script.js
@@ -25,3 +25,36 @@ var addLocation = (selectedLocation) => {
   
     localStorage.setItem("recentLocations", JSON.stringify(recentLocations));
   }
+
+  var options = {
+    startHour: 9,
+    endHour: 17,
+  }
+
+function updateTimeallocations() {
+    var currentHour = moment().hour();
+    $('.time-block').each(function (index, element){
+        var hour = $(element).attr('data-hour');
+
+        if (hour < currentHour) {
+            $(element).find('description').addClass('past');
+        }
+        else if (hour == currentHour) {
+            $(element).find('description').addClass('present');
+        }
+        else {
+            $(element).find('description').addClass('future');  
+        }
+    });
+}
+
+function onSaveTask(e) {
+    var hour = $(e.target).parent().parent().attr('data-hour');
+    var task = $(e.target).parent().prev().children().val();
+
+
+    localStorage.setItem(hour,Task);
+    console.log('saved')
+
+}
+
